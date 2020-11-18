732390e0f4a0655146de5341ed00e3ef982f7ff6
rmujumdar6 <rohitmujumdar@gatech.edu>
2020-11-17 20:48:58 -0500

    ALBERT-large code uploaded

---
+++ code/ALBERT-LARGE-V1/preprocess_data.py
@@ -0,0 +1,104 @@
+from transformers import AlbertModel, AlbertTokenizer, AdamW, get_linear_schedule_with_warmup
+import torch
+from torch.utils.data import Dataset, DataLoader,TensorDataset, DataLoader, RandomSampler, SequentialSampler
+import numpy as np
+import pandas as pd
+from torch import nn, optim
+from torch.utils.data import Dataset, DataLoader
+from pylab import rcParams
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix, classification_report
+from collections import defaultdict
+from textwrap import wrap
+import os
+import requests
+from tqdm import tqdm
+
+if not os.path.exists('../../../dataloaders'):
+    os.mkdir('../../../dataloaders')
+    
+def getDataset():
+    url = "https://www.dropbox.com/s/qjmj4wq9ywz5tb7/clean_data.csv?dl=1"
+    fname = "temp_data.csv"
+
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(fname, 'wb') as f:
+            pbar = tqdm(total=int(r.headers['Content-Length']))
+            for chunk in r.iter_content(chunk_size=8192):
+                if chunk:
+                    f.write(chunk)
+                    pbar.update(len(chunk))
+
+    aita_data = pd.read_csv("temp_data.csv")
+    os.remove("temp_data.csv")    
+    return aita_data
+
+
+def get_ids_and_attn(body, tokenizer, batch_size):	
+
+	input_ids,attention_masks = [],[]
+	
+	for i,post in enumerate(body):
+		if not(isinstance(post,str)):
+			print(i,type(post))
+		encoded_post = tokenizer.encode_plus(
+            text=post,
+            add_special_tokens=True,        # Add `[CLS]` and `[SEP]`
+			max_length=512,
+            pad_to_max_length=True,         # Pad sentence to max length
+            return_attention_mask=True      # Return attention mask
+            )
+		input_ids.append(encoded_post.get('input_ids'))
+		attention_masks.append((encoded_post.get('attention_mask')))
+	input_ids,attention_masks = torch.tensor(input_ids),torch.tensor(attention_masks)
+	return input_ids,attention_masks
+
+def preprocess_text(aita_data):
+	aita_data["body"].str.lower()
+	aita_data["body"].str.replace(r'\\n',' ', regex=True) 
+	aita_data["body"].str.replace(r"\'t", " not")
+	aita_data["body"].str.replace(r'([\;\:\|«\n])', ' ')
+	aita_data["body"].str.strip()
+
+def create_dataloader(inputs,masks,labels,category,BATCH_SIZE):
+	data = TensorDataset(inputs,masks,labels)
+	sampler = RandomSampler(data) if category=='train' else SequentialSampler(data)
+	dataloader = DataLoader(data, sampler=sampler, batch_size=BATCH_SIZE)
+	return dataloader
+
+
+if __name__=="__main__":
+	RANDOM_SEED = 40
+	loss_fn = nn.CrossEntropyLoss()
+	np.random.seed(RANDOM_SEED)
+	torch.manual_seed(RANDOM_SEED)
+	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+	BATCH_SIZE = 16
+	PRE_TRAINED_MODEL_NAME = 'albert-large-v1'
+	tokenizer = AlbertTokenizer.from_pretrained(PRE_TRAINED_MODEL_NAME)
+
+	aita_data = getDataset()
+	preprocess_text(aita_data)
+
+	df_train, df_test = train_test_split(aita_data,test_size=0.1,random_state=RANDOM_SEED)
+	df_train, df_val = train_test_split(df_train,test_size=0.5,random_state=RANDOM_SEED)
+
+	X_train,y_train,X_val,y_val = (df_train["body"].astype(str).tolist(),torch.tensor(df_train["verdict"].values),
+									df_val["body"].astype(str).tolist(),torch.tensor(df_val["verdict"].values))
+	train_labels = torch.tensor(y_train)
+	val_labels = torch.tensor(y_val)
+
+	train_inputs, train_masks = get_ids_and_attn(X_train, tokenizer, BATCH_SIZE)
+	val_inputs, val_masks = get_ids_and_attn(X_val, tokenizer, BATCH_SIZE)
+	print("Data tokenized")
+
+	train_dataloader = create_dataloader(train_inputs,train_masks,train_labels,"train",BATCH_SIZE)
+	val_dataloader = create_dataloader(val_inputs,val_masks,val_labels,"val",BATCH_SIZE)
+	
+	torch.save(train_dataloader, '../../../dataloaders/train_dataloader.pth')
+	torch.save(val_dataloader, '../../../dataloaders/val_dataloader.pth')
+	#torch.save(test_data_loader, '../dataloaders/test_dataloader.pth')
+
+	print("Dataloaders created!")
---
+++ code/ALBERT-LARGE-V1/train.py
@@ -0,0 +1,143 @@
+from transformers import AdamW, get_linear_schedule_with_warmup
+
+import random
+import time
+import numpy as np 
+import torch
+import torch.nn as nn
+
+from albert_classifier import AlbertClassifier
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+def set_seed(seed_value=42):
+    """Set seed for reproducibility.
+    """
+    random.seed(seed_value)
+    np.random.seed(seed_value)
+    torch.manual_seed(seed_value)
+    torch.cuda.manual_seed_all(seed_value)
+
+def initialize_model(epochs=4):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    albert_classifier = AlbertClassifier().to(device)
+    optimizer = AdamW(albert_classifier.parameters(),
+                      lr=5e-5,
+                      eps=1e-8)
+    train_dataloader = torch.load("../../../dataloaders/train_dataloader.pth")
+    val_dataloader = torch.load("../../../dataloaders/val_dataloader.pth")
+
+    num_steps = len(train_dataloader) * epochs
+    scheduler = get_linear_schedule_with_warmup(optimizer,num_warmup_steps=0,num_training_steps=num_steps)    
+    return albert_classifier, optimizer, scheduler,train_dataloader,val_dataloader
+
+def train(model, train_dataloader, val_dataloader=None, epochs=4, evaluation=False):
+    print("Start training...\n")
+    for epoch_i in range(epochs):
+        print(f"{'Epoch':^7} | {'Batch':^7} | {'Train Loss':^12} | {'Val Loss':^10} | {'Val Acc':^9} | {'Elapsed':^9}")
+        print("-"*70)
+
+        # Measure the elapsed time of each epoch
+        t0_epoch, t0_batch = time.time(), time.time()
+
+        # Reset tracking variables at the beginning of each epoch
+        total_loss, batch_loss, batch_counts = 0, 0, 0
+
+        # Put the model into the training mode
+        model.train()
+
+        # For each batch of training data...
+        for step, batch in enumerate(train_dataloader):
+            batch_counts +=1
+            # Load batch to GPU
+
+            b_input_ids, b_attn_mask, b_labels = tuple(t.to(device) for t in batch)
+
+            # Zero out any previously calculated gradients
+            model.zero_grad()
+
+            # Perform a forward pass. This will return logits.
+            logits = model(b_input_ids, b_attn_mask)
+
+            loss = loss_fn(logits, b_labels)
+            batch_loss += loss.item()
+            total_loss += loss.item()
+
+            loss.backward()
+
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+            optimizer.step()
+            scheduler.step()
+
+            if (step % 20 == 0 and step != 0) or (step == len(train_dataloader) - 1):
+                time_elapsed = time.time() - t0_batch
+                print(f"{epoch_i + 1:^7} | {step:^7} | {batch_loss / batch_counts:^12.6f} | {'-':^10} | {'-':^9} | {time_elapsed:^9.2f}")
+                batch_loss, batch_counts = 0, 0
+                t0_batch = time.time()
+
+        avg_train_loss = total_loss / len(train_dataloader)
+
+        print("-"*70)
+
+        if evaluation == True:
+            val_loss, val_accuracy = evaluate(model, val_dataloader)
+
+            # Print performance over the entire training data
+            time_elapsed = time.time() - t0_epoch
+            
+            print(f"{epoch_i + 1:^7} | {'-':^7} | {avg_train_loss:^12.6f} | {val_loss:^10.6f} | {val_accuracy:^9.2f} | {time_elapsed:^9.2f}")
+            print("-"*70)
+        print("\n")
+
+    torch.save(model.state_dict(),"albert_large_v1_model_trained.pth")
+    torch.save(model.state_dict(),"../../trained_models/albert_large_v1_model_trained.pth")
+    print("Training complete!")
+    
+
+
+def evaluate(model, val_dataloader):
+    """After the completion of each training epoch, measure the model's performance
+    on our validation set.
+    """
+    # Put the model into the evaluation mode. The dropout layers are disabled during
+    # the test time.
+    model.eval()
+
+    # Tracking variables
+    val_accuracy = []
+    val_loss = []
+
+    # For each batch in our validation set...
+    for batch in val_dataloader:
+        # Load batch to GPU
+        b_input_ids, b_attn_mask, b_labels = tuple(t.to(device) for t in batch)
+
+        # Compute logits
+        with torch.no_grad():
+            logits = model(b_input_ids, b_attn_mask)
+
+        # Compute loss
+        loss = loss_fn(logits, b_labels)
+        val_loss.append(loss.item())
+
+        # Get the predictions
+        preds = torch.argmax(logits, dim=1).flatten()
+
+        # Calculate the accuracy rate
+        accuracy = (preds == b_labels).cpu().numpy().mean() * 100
+        val_accuracy.append(accuracy)
+
+    # Compute the average accuracy and loss over the validation set.
+    val_loss = np.mean(val_loss)
+    val_accuracy = np.mean(val_accuracy)
+
+    return val_loss, val_accuracy
+
+if __name__ == '__main__':
+    set_seed()    # Set seed for reproducibility
+    torch. cuda. empty_cache()
+    NUM_EPOCHS=3
+    loss_fn = nn.CrossEntropyLoss()
+    albert_classifier, optimizer, scheduler,train_dataloader,val_dataloader = initialize_model(epochs=NUM_EPOCHS)
+    train(albert_classifier, train_dataloader, val_dataloader, epochs=NUM_EPOCHS, evaluation=True)
